fa526ccf177680839c59db4adafbadaf94dcd13f
ludgerradke <radke@intervall.io>
2023-02-21 19:03:56 +0100

    run black

--- pypdfmerger.py
+++ pypdfmerger.py
@@ -4,11 +4,13 @@ import sys
 
 from PyPDF2 import PdfReader, PdfWriter
 from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (
     QApplication,
+    QProgressDialog,
     QListWidget,
     QListWidgetItem,
     QWidget,
@@ -22,11 +24,11 @@ from PyQt5.QtWidgets import (
     QListView,
 )
 from pdf2image import convert_from_path
-from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
-from PyQt5.QtWidgets import QApplication, QProgressDialog
+
 
 class PdfToIcon(QThread):
     finished = pyqtSignal()
+
     def __init__(self, window, pdfs):
         super(PdfToIcon, self).__init__()
         self.window = window
@@ -36,13 +38,12 @@ class PdfToIcon(QThread):
         for file_path in self.pdfs:
             for page_num in range(len(convert_from_path(file_path))):
                 item = QListWidgetItem()
-                item.setIcon(
-                    QIcon(get_pdf_thumbnail(file_path, page_num=page_num + 1))
-                )
+                item.setIcon(QIcon(get_pdf_thumbnail(file_path, page_num=page_num + 1)))
                 item.setText(os.path.basename(file_path) + f"\n page: {page_num + 1}")
                 item.setData(Qt.UserRole, (file_path, page_num))
                 self.window.addItem(item)
         self.finished.emit()
+
 
 class FileSelectDialog(QDialog):
     def __init__(self, parent=None):
