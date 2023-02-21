430e8d0339813816cb9f60d8643ad48e73512bdd
ludgerradke <radke@intervall.io>
2023-02-21 19:29:56 +0100

    pre-commit added.

--- file_select_dialog.py
+++ file_select_dialog.py
@@ -1,7 +1,15 @@
 import os
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QFileDialog, QListWidget, QPushButton, QVBoxLayout, QHBoxLayout, QListWidgetItem
+from PyQt5.QtWidgets import (
+    QDialog,
+    QFileDialog,
+    QHBoxLayout,
+    QListWidget,
+    QListWidgetItem,
+    QPushButton,
+    QVBoxLayout,
+)
 
 
 class FileSelectDialog(QDialog):
