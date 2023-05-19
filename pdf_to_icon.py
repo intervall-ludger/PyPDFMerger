f79c67d212aca6e4ac0db67805abbb8344eaf7dc
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-05-19 11:49:33 +0100

    Loading optimized

--- pdf_to_icon.py
+++ pdf_to_icon.py
@@ -6,6 +6,8 @@ from PyQt5.QtWidgets import QListWidgetItem
 from pdf2image import convert_from_path
 
 from utils import get_pdf_thumbnail
+from PyPDF2 import PdfReader
+
 
 
 class PdfToIcon(QThread):
@@ -18,7 +20,9 @@ class PdfToIcon(QThread):
 
     def run(self):
         for file_path in self.pdfs:
-            for page_num in range(len(convert_from_path(file_path))):
+            reader = PdfReader(file_path)
+            pdf_page_count = len(reader.pages)
+            for page_num in range(pdf_page_count):
                 item = QListWidgetItem()
                 item.setIcon(QIcon(get_pdf_thumbnail(file_path, page_num=page_num + 1)))
                 item.setText(os.path.basename(file_path) + f"\n page: {page_num + 1}")
--- utils.py
+++ utils.py
@@ -6,12 +6,9 @@ from pdf2image import convert_from_path
 
 def get_pdf_thumbnail(file_path, page_num=1):
     # Convert the PDF file to a JPEG image using pdf2image
-    images = convert_from_path(file_path, dpi=50)
+    images = convert_from_path(file_path, dpi=50, first_page=page_num, last_page=page_num)
     img_bytes = io.BytesIO()
-    if page_num <= len(images):
-        images[page_num - 1].save(img_bytes, format="JPEG")
-    else:
-        return None
+    images[0].save(img_bytes, format="JPEG")
 
     # Convert the JPEG image to a QPixmap
     img_bytes.seek(0)
