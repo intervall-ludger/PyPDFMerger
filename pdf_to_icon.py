3da70ecbf605c8b5ed1f209346c7aba0e80f7029
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-08-06 16:05:45 +0200

    type annotations and docstrings added

--- pdf_to_icon.py
+++ pdf_to_icon.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 from PyPDF2 import PdfReader
 from PyQt6.QtCore import QThread, pyqtSignal, Qt
@@ -9,14 +10,30 @@ from utils import get_pdf_thumbnail
 
 
 class PdfToIcon(QThread):
+    """
+    A QThread subclass that converts PDFs to icons.
+
+    Attributes:
+        finished (pyqtSignal): Signal emitted when the thread finishes.
+    """
     finished = pyqtSignal()
 
-    def __init__(self, window, pdfs):
+    def __init__(self, window: 'QListWidget', pdfs: List[str]):
+        """
+        Initialize the PdfToIcon thread.
+
+        Args:
+            window (QListWidget): The QListWidget to add the items to.
+            pdfs (List[str]): A list of paths to the PDF files.
+        """
         super(PdfToIcon, self).__init__()
         self.window = window
         self.pdfs = pdfs
 
-    def run(self):
+    def run(self) -> None:
+        """
+        The main function of the thread. Converts each page of each PDF to an icon and adds it to the QListWidget.
+        """
         for file_path in self.pdfs:
             reader = PdfReader(file_path)
             pdf_page_count = len(reader.pages)
