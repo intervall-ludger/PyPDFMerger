089b903f85ab4db4f1e1708ffeb5e1e54d157cd6
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-08-07 06:35:07 +0200

    build action updated

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # PyInstaller for onefile
-pyinstaller --onefile --windowed --icon=icon.ico --add-data="./icon.ico;." --noconsole pypdfmerger.py --noconfirm
+pyinstaller --onefile --windowed --icon=icon.ico --add-data="./icon.ico;." --add-data="drag_and_drop.png;." --noconsole pypdfmerger.py --noconfirm
 
 # PyInstaller for onedir
-pyinstaller --onedir --windowed --icon=icon.ico --add-data="./icon.ico;." --noconsole pypdfmerger.py --noconfirm
+pyinstaller --onedir --windowed --icon=icon.ico --add-data="./icon.ico;."  --add-data="drag_and_drop.png;." --noconsole pypdfmerger.py --noconfirm
